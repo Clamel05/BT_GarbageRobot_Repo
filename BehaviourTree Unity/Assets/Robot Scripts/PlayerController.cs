4ab8638f562ee65888e1296dc7c4a9dc33de0d7a
Clamel05 <caml@bell.net>
2025-04-09 15:20:26 -0400

    Got the rotation instantiation working

--- BehaviourTree Unity/Assets/Robot Scripts/PlayerController.cs
+++ BehaviourTree Unity/Assets/Robot Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private CharacterController characterController;
+
+    public float playerSpeed;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+
+        characterController.Move(move * Time.deltaTime * playerSpeed);
+
+
+
     }
 }
--- BehaviourTree Unity/Assets/Scripts/RotateAT.cs
+++ BehaviourTree Unity/Assets/Scripts/RotateAT.cs
@@ -1,5 +1,6 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using ParadoxNotion.Serialization.FullSerializer;
 using UnityEngine;
 
 
@@ -53,7 +54,13 @@ namespace NodeCanvas.Tasks.Actions {
 			{
 				Debug.Log("hit");
 
-				//MonoBehaviour.Instantiate(laser, agent.transform.position, agent.transform.rotation);
+				Quaternion newRotation = agent.transform.rotation;
+				//Vector3 euler = new Vector3(agent.transform.rotation.x, agent.transform.rotation.y + 90, agent.transform.rotation.z);
+				//newRotation.eulerAngles = euler;
+				//newRotation.y += 90;
+				Debug.Log(newRotation.eulerAngles);
+
+				MonoBehaviour.Instantiate(laser, agent.transform.position, newRotation);
 
 				EndAction(true);
 			}
